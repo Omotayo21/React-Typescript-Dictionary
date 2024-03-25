f91c19bf7667ad5037b7bd61ea872966c050385b
Omotayo21 <110260275+Omotayo21@users.noreply.github.com>
2024-03-25 21:07:15 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
         <div className="flex items-center gap-8">
           <label
             htmlFor="toggle"
-            className={`relative block h-[2rem] w-[4rem] cursor-pointer rounded-full bg-purple-600 before:absolute before:left-2 before:top-[4px] before:h-[1.3rem] before:w-[1.3rem] before:rounded-full before:bg-white before:transition-all before:duration-300 ${
+            className={`relative block h-[1.85rem] w-[6rem] cursor-pointer rounded-full bg-purple-600 before:absolute before:left-2 before:top-[4px] before:h-[1.3rem] before:w-[1.3rem] before:rounded-full before:bg-white before:transition-all before:duration-300 ${
               dark ? "before:translate-x-full" : "before:translate-x-0"
             }`}
           >
